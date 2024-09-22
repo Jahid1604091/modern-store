568ed975c8bf1da171b2f60b120447ac420b6039
jahid1604091 <jh409780@gmail.com>
2024-09-22 15:46:14 +0600

    cors config

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,93 @@
 import asyncHandler from '../middleware/asyncHandler.js';
 import Product from '../models/productModel.js';
 
+const all_products = [
+    {
+        _id:1,
+        name:'Airpods Wirless Blutooth',
+        image:'/image/airpods.jpg',
+        description:'lorem2sdkfh sdhfjhdfk sdfjhsdkfjhsdf sdfjksdfkjdshfhdsuhfdsfdsjufsd',
+        brand:"Apple",
+        category:'Electronics',
+        price:'660',
+        countInStock:15,
+        rating:4.5,
+        numReviews:10
+    },
+    {
+        _id:2,
+        name:' Wirless Blutooth',
+        image:'/image/airpods.jpg',
+        description:'lorem2sdkfh sdhfjhdfk sdfjhsdkfjhsdf sdfjksdfkjdshfhdsuhfdsfdsjufsd',
+        brand:"Apple",
+        category:'Electronics',
+        price:'660',
+        countInStock:15,
+        rating:4.5,
+        numReviews:10
+    },
+    {
+        _id:3,
+        name:' Wirless ',
+        image:'/image/airpods.jpg',
+        description:'lorem2sdkfh sdhfjhdfk sdfjhsdkfjhsdf sdfjksdfkjdshfhdsuhfdsfdsjufsd',
+        brand:"Apple",
+        category:'Electronics',
+        price:'660',
+        countInStock:15,
+        rating:4.5,
+        numReviews:10
+    },
+    {
+        _id:4,
+        name:'Airpods Wirless Blutooth',
+        image:'/image/airpods.jpg',
+        description:'lorem2sdkfh sdhfjhdfk sdfjhsdkfjhsdf sdfjksdfkjdshfhdsuhfdsfdsjufsd',
+        brand:"Apple",
+        category:'Electronics',
+        price:'660',
+        countInStock:15,
+        rating:4.5,
+        numReviews:10
+    },
+    {
+        _id:5,
+        name:'Airpods Wirless Blutooth',
+        image:'/image/airpods.jpg',
+        description:'lorem2sdkfh sdhfjhdfk sdfjhsdkfjhsdf sdfjksdfkjdshfhdsuhfdsfdsjufsd',
+        brand:"Apple",
+        category:'Electronics',
+        price:'660',
+        countInStock:15,
+        rating:4.5,
+        numReviews:10
+    },
+    {
+        _id:6,
+        name:'Airpods Wirless Blutooth',
+        image:'/image/airpods.jpg',
+        description:'lorem2sdkfh sdhfjhdfk sdfjhsdkfjhsdf sdfjksdfkjdshfhdsuhfdsfdsjufsd',
+        brand:"Apple",
+        category:'Electronics',
+        price:'660',
+        countInStock:15,
+        rating:4.5,
+        numReviews:10
+    },
+    {
+        _id:7,
+        name:'Airpods Wirless Blutooth',
+        image:'/image/airpods.jpg',
+        description:'lorem2sdkfh sdhfjhdfk sdfjhsdkfjhsdf sdfjksdfkjdshfhdsuhfdsfdsjufsd',
+        brand:"Apple",
+        category:'Electronics',
+        price:'660',
+        countInStock:15,
+        rating:4.5,
+        numReviews:10
+    },
+]
+
 //@route    /api/products?q='df'
 //@desc     get all products
 //@access   public
@@ -17,7 +104,7 @@ export const getAllProducts = asyncHandler(async (req, res) => {
     // const count = await Product.count({ ...query })
     const products = await Product.find({ ...query }).limit(per_page).skip(per_page * (page - 1));
     res.status(200).json({
-        products,
+        products:all_products,
         page,
         // pages: Math.ceil(count / per_page)
     })
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,30 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { connectDB } from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { errHandler, notFound } from './middleware/errorHandler.js';
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
+
 connectDB();
+app.use(cors({
+    credentials: true,
+    origin: [process.env.DEV_DOMAIN,process.env.LIVE_DOMAIN],
+    // origin: "*",
+    methods: ['GET','POST','PUT','PATCH','DELETE'],
+    allowedHeaders: ['Content-Type',],
+  }));
+
 app.get('', (req, res) => {
     res.send('Server is up...')
 });
+
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
