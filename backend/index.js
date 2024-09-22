332d83efe2f09d2b659a4db902d3ed6bdfe078df
jahid1604091 <jh409780@gmail.com>
2024-09-22 13:19:34 +0600

    db conenction

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
 
-const PORT = 5000;
+dotenv.config();
+const PORT =process.env.PORT || 5000;
 const app = express();
-
+connectDB();
 app.get('',(req,res)=>{
     res.send('Server is up...')
 });
